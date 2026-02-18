f5db413531f13f0c4d30ef212837a7f6a493e7a2
shreyas0957 <shreyaspatelmpbca25@rvu.edu.in>
2026-02-18 11:15:57 +0530

    Add files via upload

---
+++ Shreyasp1.py
@@ -0,0 +1,6 @@
+set={"Shreyas","Apple","Caroot"}
+set.add("kumar")
+set.update(["Rohit","Sharma"])
+set.remove("kumar")
+set.discard("orange")
+print(set)
---
+++ Shreyasp2.py
@@ -0,0 +1,42 @@
+import sys
+def list_operations():
+ my_list=[]
+ while True:
+         print("\nList Operations")
+         print("1.Insert an element")
+         print("2.Delete an element")
+         print("3.Find an element")
+         print("4.Display list")
+         print("5.Exit")
+         choice=int(input("Enter your choice:"))
+         if choice == 1:
+            element=input("Enter element to insert:")
+            my_list.append(element)
+            print("Element'(element)'inserted")
+             
+         elif choice == 2:
+            element=input("Enter element to delete:")
+            if element in my_list:
+                my_list.remove(element)
+                print(f"Element'(element)'deleted")
+            else:
+                print("Element'(element)'not found")
+         elif choice == 3:
+             element=input("Enter element to find:")
+             if element in my_list:
+                 print("Element'(element)'found")
+             else:
+                 print("Element'(element)'not found")
+                 
+         elif choice == 4:
+             print("Cannot list: (my_list)")
+             
+         elif choice == 5:
+             print("Exiting program")
+             sys.exit
+             break
+            
+         else:
+                print("Invalid choice:please try again")
+list_operations()
+             
